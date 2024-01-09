2d4e533ede2acc48feb71b393f0ea885c3d0a967
Georgios Kopanidis <kopanidis@me.com>
2024-01-09 12:02:56 +0000

    Initial commit

---
+++ student.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+class Student:
+    """
+    A Student class as base for method testing
+    """
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
