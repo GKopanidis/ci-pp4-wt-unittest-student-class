41112abb74bb29c5a9e11a0910cc35eb5986d419
Georgios Kopanidis <kopanidis@me.com>
2024-01-09 13:01:08 +0000

    Mocking the method

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -24,3 +25,11 @@ class Student:
 
     def apply_extension(self, days):
         self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
     
@@ -36,7 +37,25 @@ class TestStudent(unittest.TestCase):
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
-        
+
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
+
+
 
 if __name__ == "__main__":
     unittest.main()
