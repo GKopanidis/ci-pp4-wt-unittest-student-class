ad987626c97216a894f4438ba599613b2ab6ded5
Georgios Kopanidis <kopanidis@me.com>
2024-01-09 12:46:12 +0000

    ADd test_apply_extension test

--- student.py
+++ student.py
@@ -1,7 +1,8 @@
 from datetime import date, timedelta
 
+
 class Student:
-    """ A Student class as base for method testing """
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -10,16 +11,16 @@ class Student:
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
-
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
-
 
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
-    
-    def altert_santa(self):
+    def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
--- test_student.py
+++ test_student.py
@@ -1,45 +1,42 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 class TestStudent(unittest.TestCase):
-
+    
     @classmethod
     def setUpClass(cls):
-        print('setUpClass')
+        print("set up class")
 
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
 
     @classmethod
     def tearDownClass(cls):
-        print('tearDownClass')
-
-
-    def setUp(self):
-        print('setUp')
-        self.student = Student('John', 'Doe')
-
+        print("tear down Class")
 
     def tearDown(self):
-        print('tearDown')
-
+        print("tear down")
 
     def test_full_name(self):
-        print('test_full_name')
-
-        self.assertEqual(self.student.full_name, 'John Doe')
-
-
-    def test_email(self):
-        print('test_email')
-
-        self.assertEqual(self.student.email, 'john.doe@email.com')
-
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        print('test_alert_santa')
-        self.student.altert_santa()
-
+        print("test_alert_santa")
+        self.student.alert_santa()
         self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
         
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
